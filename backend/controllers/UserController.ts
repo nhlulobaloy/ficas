77371ad46e4999b6abbd04bfadfab550874abc3e
Nhlulo Baloy <nhlulobaloy22@gmail.com>
2026-08-25 15:18:46 +0200

    refactor: move user database logic to models layer

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -1,20 +1,13 @@
-import pool from '../config/db.js';
 import { Request, Response } from 'express';
+import { getAllUsers } from '../models/userModel.js';
 
-
-//get all the user
+// get all the user
 export const getUsers = async (req: Request, res: Response) => {
     try {
-        const sql = `SELECT 
-                       id,
-                       name,
-                       email,
-                       role 
-                       FROM users`;// do not select the password
-        const [results] = await pool.execute(sql)    
+        const results = await getAllUsers();
         return res.json(results)
     } catch (error) {
         console.error("DB Error:", error);
-        return res.status(500).json({ error: "Failed to get users" });
+        return res.status(500).json({ error: "Internal server error" });
     }
 }
